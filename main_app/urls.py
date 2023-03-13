c90bb6f048f1f0fb15249b112985d61ec75196e6
isobelmorales <isofmorales@gmail.com>
2023-03-13 16:24:13 -0400

    can create transactions

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     # home
     path('', views.home, name='home'),
     # transactions index
-    path('transactions/', views.transactions_index, name='transactions_index')
+    path('transactions/', views.transactions_index, name='transactions_index'),
+    # create transaction
+    path('transactions/create', views.TransactionCreate.as_view(), name='transactions_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Transaction
 
 # Create your views here.
@@ -12,3 +13,11 @@ def transactions_index(request):
     # collecting transaction model/info from SQL
     transactions = Transaction.objects.all()
     return render(request, 'transactions/index.html', { 'transactions': transactions })
+
+# Create Transaction
+class TransactionCreate(CreateView):
+    model = Transaction
+    # including all fields
+    fields = '__all__'
+    # redirecting to index page upon successful creation
+    success_url = '/transactions/'
