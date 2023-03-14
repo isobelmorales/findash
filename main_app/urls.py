1e2883b09406976afdd4e2c2d9d0cf3d2712a926
isobelmorales <isofmorales@gmail.com>
2023-03-14 00:24:18 -0400

    account and budget index urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('transactions/', views.transactions_index, name='transactions_index'),
     # add transaction
     path('transactions/add_transaction/', views.add_transaction, name='add_transaction'),
+    # accounts index
+    path('accounts/', views.accounts_index, name='accounts_index'),
+    # budgets index
+    path('budget/', views.budget_index, name='budget_index'),
 ]
