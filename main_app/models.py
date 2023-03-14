01d715903b1b6d5273791ca39cda00897153c49a
isobelmorales <isofmorales@gmail.com>
2023-03-14 00:22:44 -0400

    account model and migrations

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+# Account
+class Account(models.Model):
+    name = models.CharField(max_length=50)
+    balance = models.IntegerField()
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 # Transaction
 class Transaction(models.Model):
     name = models.CharField(max_length=100)
