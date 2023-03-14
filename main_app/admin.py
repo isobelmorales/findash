dfe1c564b4e836476583e7efa723387efa29b66e
isobelmorales <isofmorales@gmail.com>
2023-03-14 02:58:35 -0400

    updates styling

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Import your models here.
-from .models import Transaction
+from .models import Transaction, Account, Budget
 
 # Register your models here.
 admin.site.register(Transaction)
+admin.site.register(Account)
+admin.site.register(Budget)
